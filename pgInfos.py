8eabf41d69c55808ef39e584908981cc4607f3a8
Anderson Arruda <andmarruda@gmail.com>
2022-10-20 20:23:53 -0300

    Update pgInfos.py

--- pgInfos.py
+++ pgInfos.py
@@ -1,13 +1,46 @@
 import psycopg2
 
 class sqls:
+    __filterNameSpace = "NOT pn.nspname IN('pg_catalog', 'information_schema', 'pg_toast')"
+    __filterClass = "relpersistence='p'"
+    __pgClass = """
+        SELECT 
+            pn.nspname || '.' || pc.relname as obj 
+        FROM 
+            pg_class pc 
+            JOIN pg_namespace pn ON pn.oid=pc.relnamespace 
+        WHERE pc.relkind IN ({}) AND {} AND {}
+    """
+    __pgAttribute = """
+        SELECT
+            pn.nspname, pc.relname, pa.attname, pa.attlen, pa.attnum, 
+            pa.attndims, pa.attcacheoff, pa.atttypmod, pa.attstorage, pa.attcompression,
+            pa.attnotnull, pa.atthasdef, pa.atthasmissing, pa.attidentity, pa.attgenerated,
+            
+            pt.typname, pt.typlen, pt.typbyval, pt.typtype, pt.typcategory 
+        FROM
+            pg_attribute pa
+            JOIN pg_class pc ON pc.oid=pa.attrelid
+            JOIN pg_namespace pn ON pn.oid=pc.relnamespace
+            JOIN pg_type pt ON pt.oid=pa.atttypid
+        WHERE
+            pa.attnum > 0 AND NOT attisdropped AND pn.nspname || '.' || pc.relname = %s
+    """
+
     def __init__(self, host, port, user, passw, dbname):
         self.__conn = psycopg2.connect(host=host, database=dbname, port=port, user=user,
                                        password=passw)
     def getAllPGClass(self, relkind):
         cur = self.__conn.cursor()
-        cur.execute("SELECT pn.nspname || '.' || pc.relname as obj FROM pg_class pc JOIN pg_namespace pn ON pn.oid=pc.relnamespace WHERE pc.relkind IN ("+ ('%s, ' * len(relkind)).rstrip(', ') +") AND NOT pn.nspname IN('pg_catalog', 'information_schema', 'pg_toast') AND relpersistence='p'", relkind)
-        return list(map(lambda cls: cls[0], cur.fetchall()))
+        cls = type(self)
+        cur.execute(cls.__pgClass.format(('%s, ' * len(relkind)).rstrip(', '), cls.__filterNameSpace, cls.__filterClass), relkind)
+        return list(map(lambda pgcls: pgcls[0], cur.fetchall()))
+
+    def getAllAttribute(self, relname):
+        cur = self.__conn.cursor()
+        cls = type(self)
+        cur.execute(cls.__pgAttribute, [relname])
+        return cur.fetchall()
 
     def pgClassWithRowCount(self):
         allcls = self.getAllPGClass(['r', 'm'])
